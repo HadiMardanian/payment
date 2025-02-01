3d85b39a1e5c7c2fa7eee34ddab579aaf54bd240
Hadi Mardanian <mardanianhadi@gmail.com>
2025-02-01 15:03:08 +0330

    feat: Add database model and repository

---
+++ src/models/Payment.ts
@@ -0,0 +1,31 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { HydratedDocument } from 'mongoose';
+
+export type PaymentDocument = HydratedDocument<Payment>;
+
+@Schema({ timestamps: { createdAt: true, updatedAt: true } })
+export class Payment {
+    @Prop({ required: true })
+    gateway: "zarinpal" | "shepa";
+
+    @Prop({ required: true })
+    authority: string;
+
+    @Prop({ required: true })
+    amount: number;
+
+    @Prop({ required: true })
+    description: string;
+
+    @Prop()
+    mobile?: string;
+
+    @Prop()
+    email?: string;
+
+    @Prop({ required: true, enum: ["pending", "failed", "success", "canceled"] })
+    status: "pending" | "failed" | "success" | "canceled";
+
+}
+
+export const PaymentModel = SchemaFactory.createForClass(Payment);
