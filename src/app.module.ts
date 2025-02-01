04d70b0eea4fb2337a86f3e7ac714d949d58c4ca
Hadi Mardanian <mardanianhadi@gmail.com>
2025-02-01 15:04:47 +0330

    feat: Update app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,17 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { PaymentModule } from './modules/payment.module';
+import { Payment, PaymentModel } from './models/Payment';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot(),
+    MongooseModule.forRoot(process.env.MONGO_CONNECTION_STRING!),
+    MongooseModule.forFeature([{ name: Payment.name, schema: PaymentModel }]),
+    PaymentModule
+  ],
+  controllers: [],
+  providers: [],
 })
-export class AppModule {}
+export class AppModule { }
