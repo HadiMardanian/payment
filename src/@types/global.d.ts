6203152afc2e60081cf2588e340523b208d7be33
Hadi Mardanian <mardanianhadi@gmail.com>
2025-02-02 12:33:20 +0330

    feat: Update types

--- src/@types/global.d.ts
+++ src/@types/global.d.ts
@@ -1,9 +1,10 @@
 namespace NodeJS {
     interface ProcessEnv {
         PORT: string;
-        CALLBACK_URL: string;
 
+        CALLBACK_URL: string;
         MERCHANT_ID: string;
+        MAX_PAYMENT_LIMIT: string;
 
         SHEPA_URL: string;
         SHEPA_APIKEY: "sandbox";
