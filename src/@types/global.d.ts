af16757e8d1e911c052cd698b8f4599f20214265
Hadi Mardanian <mardanianhadi@gmail.com>
2025-02-02 16:37:24 +0330

    feat: Update types

--- src/@types/global.d.ts
+++ src/@types/global.d.ts
@@ -5,9 +5,11 @@ namespace NodeJS {
         CALLBACK_URL: string;
         MERCHANT_ID: string;
         MAX_PAYMENT_LIMIT: string;
-
+        READY_TO_PAY_LINK: string;
+        DEFAULT_GATEWAY: "zarinpal" | "shepa";
+        
         SHEPA_URL: string;
-        SHEPA_APIKEY: "sandbox";
+        SHEPA_APIKEY: string;
 
         ZARINPAL_URL: string;
         ZARINPAL_TOKEN: string;
